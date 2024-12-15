b6ce2230111e38b0158481686b041b0ad91e4a8a
Mohamed Taha <mtaha@pp.gov.qa>
2024-12-15 21:42:59 +0300

    llama3:latest

--- search.py
+++ search.py
@@ -8,9 +8,9 @@ queryembed = ollama.embed(model="nomic-embed-text", input=query)['embeddings']
 
 relateddocs = '\n\n'.join(collection.query(query_embeddings=queryembed, n_results=10)['documents'][0])
 prompt = f"{query} - Answer that question using the following text as a resource: {relateddocs}"
-noragoutput = ollama.generate(model="llama3.3:70b", prompt=query, stream=False) # mistral
-print(f"Answered without RAG [llama3.3:70b]:: {noragoutput['response']}")
+noragoutput = ollama.generate(model="llama3:latest", prompt=query, stream=False) # mistral
+print(f"Answered without RAG [llama3:latest]:: {noragoutput['response']}")
 print("########################")
-ragoutput = ollama.generate(model="llama3.3:70b", prompt=prompt, stream=False)
+ragoutput = ollama.generate(model="llama3:latest", prompt=prompt, stream=False)
 
-print(f"Answered with RAG [llama3.3:70b]:: {ragoutput['response']}")
+print(f"Answered with RAG [llama3:latest]:: {ragoutput['response']}")
